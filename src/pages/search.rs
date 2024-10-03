1f7b7a2b07a6a091b78096127082b6e87bb3a48e
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-10-04 08:56:37 +0900

    銘柄コード検索機能を追加

--- src/pages/search.rs
+++ src/pages/search.rs
@@ -1,41 +1,68 @@
 use crate::components::Layout;
-use web_sys::{HtmlInputElement, InputEvent};
+use web_sys::InputEvent;
 use yew::prelude::*;
 
-#[function_component(Search)]
-pub fn search() -> Html {
-    let search_input = use_state(|| String::new());
-    let kabutan_link = use_state(|| String::new());
+pub struct Search {
+    stock: String,
+}
 
-    let oninput = {
-        let search_input = search_input.clone();
-        let kabutan_link = kabutan_link.clone();
-        Callback::from(move |e: InputEvent| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            let value = input.value();
-            search_input.set(value.clone());
-            let link = format!("https://kabutan.jp/stock/?code={}", value);
-            kabutan_link.set(link);
-        })
-    };
+pub enum Msg {
+    UpdateStock(String),
+}
 
-    html! {
-        <Layout>
-            <h2 class="mb-4">{ "銘柄検索" }</h2>
-            <div class="mb-3">
-                <label for="stockCode" class="form-label">{ "銘柄コード" }</label>
-                <input
-                    type="text"
-                    class="form-control"
-                    id="stockCode"
-                    placeholder="銘柄コードを入力"
-                    value={(*search_input).clone()}
-                    oninput={oninput}
-                />
-            </div>
-            <div class="mt-3">
-                <a href={(*kabutan_link).clone()} target="_blank" class="btn btn-primary">{ "かぶたんで確認する" }</a>
-            </div>
-        </Layout>
+impl Component for Search {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            stock: "".to_string(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::UpdateStock(code) => {
+                self.stock = code;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let oninput = ctx.link().callback(|e: InputEvent| {
+            let input: web_sys::HtmlInputElement = e.target_unchecked_into();
+            Msg::UpdateStock(input.value())
+        });
+
+        // 各ウェブサイトへのリンクを作成
+        let kabutan_link = format!("https://kabutan.jp/stock/?code={}", self.stock);
+        let yahoo_finance_link = format!("https://finance.yahoo.co.jp/quote/{}", self.stock);
+        let nikkei_link = format!("https://www.nikkei.com/nkd/company/?scode={}", self.stock);
+        let buffett_code_link = format!("https://www.buffett-code.com/company/{}", self.stock);
+        let minkabu_link = format!("https://minkabu.jp/stock/{}/", self.stock);
+
+        html! {
+            <Layout>
+                <h2 class="mb-4">{ "銘柄検索" }</h2>
+                <div class="mb-3">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="stockCode"
+                        placeholder="銘柄名・銘柄コードを入力"
+                        value={self.stock.clone()}
+                        oninput={oninput}
+                    />
+                </div>
+                <div class="mt-3">
+                    <a href={kabutan_link} target="_blank" class="btn btn-primary me-2">{ "かぶたんで確認する" }</a>
+                    <a href={yahoo_finance_link} target="_blank" class="btn btn-secondary me-2">{ "Yahoo! Financeで確認する" }</a>
+                    <a href={nikkei_link} target="_blank" class="btn btn-success me-2">{ "日経で確認する" }</a>
+                    <a href={buffett_code_link} target="_blank" class="btn btn-warning me-2">{ "バフェットコードで確認する" }</a>
+                    <a href={minkabu_link} target="_blank" class="btn btn-info">{ "みんかぶで確認する" }</a>
+                </div>
+            </Layout>
+        }
     }
 }
