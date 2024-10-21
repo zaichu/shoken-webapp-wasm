dce9b96f3f2205576fd1ef7cd887f0b1398823dc
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-10-22 08:35:39 +0900

    GitHubPagesのSPA対応がんばってまうｓ．

--- src/app.rs
+++ src/app.rs
@@ -29,17 +29,16 @@ pub fn switch(routes: Route) -> Html {
 pub fn app() -> Html {
     use_effect(|| {
         if let Some(window) = web_sys::window() {
-            if let Some(storage) = window.session_storage().ok().flatten() {
-                if let Ok(Some(redirect)) = storage.get_item("redirect") {
-                    let _ = storage.remove_item("redirect");
-                    if let Some(history) = window.history().ok() {
-                        let _ = history.replace_state_with_url(&JsValue::NULL, "", Some(&redirect));
-                    }
-                }
+            let location = window.location();
+            let pathname = location.pathname().unwrap_or_default();
+            if pathname != "/shoken-webapp-wasm/" {
+                let history = window.history().unwrap();
+                let _ = history.replace_state_with_url(&JsValue::NULL, "", Some(&pathname));
             }
         }
         || ()
     });
+
     html! {
         <BrowserRouter>
             <Switch<Route> render={switch} />
