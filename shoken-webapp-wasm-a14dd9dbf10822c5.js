c68e817704eb34bc9bd3fcb951ca201068003858
zaichu <zaichu@users.noreply.github.com>
2024-11-04 07:37:16 +0000

    deploy: 8d66740677f7f09f70dc12cc3e2284a33828d6ce

--- shoken-webapp-wasm-a14dd9dbf10822c5.js
+++ shoken-webapp-wasm-a14dd9dbf10822c5.js
@@ -183,15 +183,15 @@ function makeMutClosure(arg0, arg1, dtor, f) {
     return real;
 }
 function __wbg_adapter_40(arg0, arg1, arg2) {
-    wasm.closure569_externref_shim(arg0, arg1, arg2);
+    wasm.closure565_externref_shim(arg0, arg1, arg2);
 }
 
 function __wbg_adapter_43(arg0, arg1, arg2) {
-    wasm.closure630_externref_shim(arg0, arg1, arg2);
+    wasm.closure626_externref_shim(arg0, arg1, arg2);
 }
 
 function __wbg_adapter_46(arg0, arg1, arg2) {
-    wasm.closure650_externref_shim(arg0, arg1, arg2);
+    wasm.closure646_externref_shim(arg0, arg1, arg2);
 }
 
 export function main() {
@@ -900,15 +900,15 @@ function __wbg_get_imports() {
         return ret;
     };
     imports.wbg.__wbindgen_closure_wrapper1212 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 570, __wbg_adapter_40);
+        const ret = makeMutClosure(arg0, arg1, 566, __wbg_adapter_40);
         return ret;
     };
     imports.wbg.__wbindgen_closure_wrapper1394 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 631, __wbg_adapter_43);
+        const ret = makeMutClosure(arg0, arg1, 627, __wbg_adapter_43);
         return ret;
     };
     imports.wbg.__wbindgen_closure_wrapper1433 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 651, __wbg_adapter_46);
+        const ret = makeMutClosure(arg0, arg1, 647, __wbg_adapter_46);
         return ret;
     };
     imports.wbg.__wbindgen_init_externref_table = function() {
