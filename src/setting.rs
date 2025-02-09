816c2b71ebf9a38faeb9392b34e21ac4ff629428
zaichu <zaitomo41@gmail.com>
2025-02-09 23:30:18 +0900

    コメントのインデントを揃える

--- src/setting.rs
+++ src/setting.rs
@@ -39,33 +39,33 @@ lazy_static! {
     };
 
     pub static ref NUMBER_FORMAT_KEYS: HashSet<&'static str> = [
-        "shares",                         // 数量
+        "shares", // 数量
     ].iter().cloned().collect();
 
     pub static ref YEN_FORMAT_KEYS: HashSet<&'static str> = [
-        "asked_price",                        // 売却/決済単価
-        "dividends_before_tax",               // 配当・分配金(税引前)
-        "net_amount_received",                // 受取金額
-        "proceeds",                           // 売却/決済額
-        "total_realized_profit_and_loss_after_tax",                    // 損益
-        "purchase_price",                     // 平均取得価額
-        "realized_profit_and_loss",           // 実現損益
-        "taxes",                              // 税額
-        "total_dividends_before_tax",         // 配当・分配金合計(税引前)
-        "total_net_amount_received",          // 受取金額合計
-        "total_realized_profit_and_loss",     // 合計実現損益
-        "total_taxes",                        // 税額合計
-        "withholding_tax",                    // 源泉徴収税額
-        "unit_price",                         // 単価
-        "cancellation_unit_price_yen",        // 解約単価［円］
-        "cancellation_amount_yen",            // 解約額［円］
-        "average_acquisition_price_yen",      // 平均取得価額［円］
-        "realized_profit_and_loss_after_tax", // 実現損益(税抜)
+        "asked_price",                              // 売却/決済単価
+        "dividends_before_tax",                     // 配当・分配金(税引前)
+        "net_amount_received",                      // 受取金額
+        "proceeds",                                 // 売却/決済額
+        "total_realized_profit_and_loss_after_tax", // 損益
+        "purchase_price",                           // 平均取得価額
+        "realized_profit_and_loss",                 // 実現損益
+        "taxes",                                    // 税額
+        "total_dividends_before_tax",               // 配当・分配金合計(税引前)
+        "total_net_amount_received",                // 受取金額合計
+        "total_realized_profit_and_loss",           // 合計実現損益
+        "total_taxes",                              // 税額合計
+        "withholding_tax",                          // 源泉徴収税額
+        "unit_price",                               // 単価
+        "cancellation_unit_price_yen",              // 解約単価［円］
+        "cancellation_amount_yen",                  // 解約額［円］
+        "average_acquisition_price_yen",            // 平均取得価額［円］
+        "realized_profit_and_loss_after_tax",       // 実現損益(税抜)
     ].iter().cloned().collect();
 
     pub static ref DATE_FORMAT_KEYS: HashSet<&'static str> = [
-        "settlement_date",                // 受取金
-        "trade_date",                     // 約定日
+        "settlement_date", // 受取金
+        "trade_date",      // 約定日
     ].iter().cloned().collect();
 
     pub static ref STOCK_INFO_LINKS: Vec<(&'static str, &'static str)> = vec![
